f950ff4f5645f16ea15d9ef7d6ba2dba8f27c63e
Ross Harrison <rossharrison@macbookpro.lan>
2026-03-31 13:13:16 -0400

    Version 2: Formatting and UI added

--- message-generator/script.js
+++ message-generator/script.js
@@ -1,20 +1,26 @@
-// Part 1: The Data Store
-const messageComponents = {
-  opening: ['Today', 'Tomorrow', 'In the near future', 'Next week'],
-  action: ['you will find', 'you should avoid', 'you will master', 'you will encounter'],
-  object: ['a mysterious cat', 'a coding breakthrough', 'a cold cup of coffee', 'unlimited inspiration']
+const messageStore = {
+  destination: ['Paris', 'The Moon', 'A Hidden Temple'],
+  companion: ['a friendly robot', 'a stray cat', 'a wise hermit'],
+  motive: ['to find gold', 'to seek inner peace', 'to learn a secret recipe']
 };
 
-// Part 2: The Logic Function
-function generateMessage() {
-  // We grab a random element from each array
-  const part1 = messageComponents.opening[Math.floor(Math.random() * messageComponents.opening.length)];
-  const part2 = messageComponents.action[Math.floor(Math.random() * messageComponents.action.length)];
-  const part3 = messageComponents.object[Math.floor(Math.random() * messageComponents.object.length)];
-
-  // Part 3: Return the combined string
-  return `${part1}, ${part2} ${part3}.`;
+function displayMessage() {
+  console.log("========================================");
+  console.log("   --- THE ADVENTURE GENERATOR ---   ");
+  console.log("========================================");
+  
+  let finalMessage = [];
+  
+  // Example of using a loop for scalability
+  for (let part in messageStore) {
+    let index = Math.floor(Math.random() * messageStore[part].length);
+    finalMessage.push(messageStore[part][index]);
+  }
+  
+  console.log(`Target: ${finalMessage[0]}`);
+  console.log(`With: ${finalMessage[1]}`);
+  console.log(`Mission: ${finalMessage[2]}`);
+  console.log("========================================");
 }
 
-// Part 4: Output to the console
-console.log(generateMessage());
+displayMessage();
